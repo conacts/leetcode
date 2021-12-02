635df5254ff2284f9c99624723719d21bb9fbcfe
cs <csheehan630@gmail.com>
2021-12-01 22:26:41 -0500

    advent of code day 1

---
+++ advent_of_code/2021/day1.py
@@ -0,0 +1,30 @@
+# my horrible implementations
+
+def part1(file):
+    count = 0
+    prev_line = 0
+    with open(file) as f:
+        out = f.readlines()
+        out = f.readlines()
+        for line in out:
+            line = int(line.strip('\n'))
+            if prev_line < line:
+                count+= 1
+            prev_line = line
+
+        return count-1
+
+def part2(file):
+    with open(file) as f:
+        lines = list(map(int, f.read().splitlines()))
+        prev_line = sum(lines[:2])
+        count = 0
+        s = 3
+        while s < len(lines):
+            line = sum(lines[s-3:s])
+            if prev_line < line:
+                count += 1
+            s += 1
+            prev_line = line
+
+    return count
