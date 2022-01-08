dbd2bba4e70b282888376c065af3858aa37646e8
cs <csheehan630@gmail.com>
2022-01-08 00:58:24 -0500

    twosum2 C++

---
+++ leetcode/tree/insert_in_bst.cpp
@@ -0,0 +1,12 @@
+TreeNode* insertIntoBST(TreeNode* root, int val) {
+	if (!root)
+	{
+		TreeNode* newNode = new TreeNode(val);
+		return newNode;
+	}
+	if (root->val < val)
+		root->right = insertIntoBST(root->right, val);
+	else
+		root->left = insertIntoBST(root->left, val);
+	return root;
+}
